848791c548b310102236c03745764af09f320d04
Ben <ben.simmons@trinityis.com>
2014-06-07 20:05:35 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Benmarks::Application.config.secret_key_base = '3efab6498c14c044c7d2011c6d07921070f95c6475ba27afd1a74dc66a25232225b24de155408b0f3618cd8e1df5350ef105c3e13daf0cfa796660aa92f85777'
